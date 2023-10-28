1612eb403fee210bc63af043d8bbba410f9ef9c0
prographicsten <prographicsten@gmail.com>
2023-10-28 11:51:35 +0600

    check out findOne server code add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    // find a specific services
+    app.get('/services/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {  _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    }); 
     // -------------------------------------------------------------
 
 
