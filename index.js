0ee67e0c43663d2e0e5fb1418fe18a0ffda55282
prographicsten <prographicsten@gmail.com>
2023-10-28 12:56:50 +0600

    order codes add

--- index.js
+++ index.js
@@ -34,7 +34,11 @@ async function run() {
 
     // -------------------------------------------------------------
     const serviceCollection = client.db('carDoctor').collection('services');
-
+    const bookingCollection = client.db('carDoctor').collection('bookings');
+    // -------------------------------------------------------------
+    
+    
+    // -------------------------------------------------------------
     // find all services
     app.get('/services', async(req, res) => {
       const cursor = serviceCollection.find();
@@ -46,9 +50,24 @@ async function run() {
     app.get('/services/:id', async(req, res) => {
       const id = req.params.id;
       const query = {  _id: new ObjectId(id) };
-      const result = await serviceCollection.findOne(query);
+      
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+
+
+      const result = await serviceCollection.findOne(query, options);
       res.send(result);
     }); 
+
+    // bookings order
+    app.post('/bookings', async(req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
     // -------------------------------------------------------------
 
 
