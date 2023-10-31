b23bca843d060831fec334b659e25330ac7bc86c
prographicsten <prographicsten@gmail.com>
2023-10-31 16:29:48 +0600

    new comment add

--- __jwt__steps__.js
+++ __jwt__steps__.js
@@ -32,3 +32,27 @@
  * 2. use cookie parser as middleware 
  * 
  */ 
+
+
+// recaps
+/**
+ * ---------------------------
+ *       Make Api Secure
+ * ---------------------------
+ * 
+ * The person who should have
+ * 
+ * concept: 
+ * 1. assign two tokens for each person (access token, refresh token)
+ * 2. access token contains: user identification (email, role, etc). valid for a shorter duration
+ * 3. refresh token is used: to recreate an access token that was expired.
+ * 4. if refresh is invalid then logout the user
+ * 
+ * 
+ */
+
+
+/**
+ * 1. jwt  --> json web token
+ * 
+ */
